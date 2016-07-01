47764e477958fe9e09180a0cad0feab739764baf
anandnk24 <anandnk24@gmail.com>
2016-07-01 00:01:14 -0500

    few more changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    $(".arrow").hover(function() {
+    $(".arrow").click(function() {
         $('html, body').animate({
         	scrollTop: $(".col-md-12").offset().top
-        }, 1500);
+        }, 1100);
 
     });
 });
