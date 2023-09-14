43e66b4ad3c7e10d5c789dd36a3210305a5ade02
Rebecca Qian <rebeccaqian@uchicago.edu>
2023-09-14 10:33:08 -0400

    new assets for site

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+	var page = $('html, body');
+
+    $(".arrow").click(function() {
+
+       	page.on("scroll mousedown DOMMouseScroll keyup mousewheel touchmove", function(){
+       		page.stop();
+   	   	});
+
+        page.animate({
+        	scrollTop: $(".col-md-12").offset().top
+        }, 1200, function() {
+        	page.off("scroll mousedown DOMMouseScroll keyup mousewheel touchmove");
+        });
+
+        return false;
+    });
+
+});
+
+// page.animate({
+//         	scrollTop: $(".col-md-12").offset().top}
+//         }, 1200);
+
+//mousedown wheel DOMMouseScroll mousewheel keyup touchmove
